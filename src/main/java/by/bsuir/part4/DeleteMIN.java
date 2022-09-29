ebbe3c5a21bec92df47629f60db6d4fe892c5b2e
Elizabeth <liza.ants@gmail.com>
2022-09-29 15:21:05 +0300

    add tak4

--- src/main/java/by/bsuir/part4/DeleteMIN.java
+++
@@ -1,2 +0,0 @@
-package by.bsuir.part4;public class DeleteMIN {
-}
---
+++ src/main/java/by/bsuir/part4/Transaction.java
@@ -0,0 +1,33 @@
+package by.bsuir.part4;
+
+import java.util.Date;
+
+
+public class Transaction {
+
+    private final String who;
+    private final Date when;
+    private final double amount;
+
+    private  int hash;
+
+
+
+    public Transaction(String who, Date when, double amount, int hash) {
+        this.who = who;
+        this.when = when;
+        this.amount = amount;
+        this.hash = hash;
+    }
+
+    public int hashCode(){
+        hash = 17;
+        hash = 31 * hash + who.hashCode();
+        hash = 31 * hash + when.hashCode();
+        hash = 31 * hash + ((Double)amount).hashCode();
+        return hash;
+    }
+
+
+
+}
